3c8482aede962ab2072de8a35fc736f5be2b5942
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-03-06 11:26:57 +0100

    initial commit

---
+++ prelude.fsx
@@ -0,0 +1,11 @@
+open System
+
+// https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+let DIM       = "\u001b[2m"
+let DIM_RESET = "\u001b[22m"
+
+fsi.AddPrinter(fun (x: DateTimeOffset) ->
+    let part1 = x.ToString "yyyy-MM-ddTHH:mm:ss"
+    let part2 = x.ToString ".fffffff"
+    let part3 = x.ToString "zzz"
+    sprintf "%s%s%s%s%s" part1 DIM part2 DIM_RESET part3)
